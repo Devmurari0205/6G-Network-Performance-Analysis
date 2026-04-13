d369deeb4e486bc3d8313b4b4ebe479aac1a343e
Harsh Devmurari <harshdevmurari08@gmail.com>
2026-04-13 16:54:14 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -321,17 +321,22 @@ col3.metric("Avg Error Rate", round(df['error_rate'].mean(), 2))
 col1, col2, col3 = st.columns(3)
 
 # Speed by Latency Band
-speed_band = df.groupby('latency_band')['production_speed_units_per_hr'].mean().reset_index()
+if 'latency_band' in df.columns and 'production_speed_units_per_hr' in df.columns:
 
-fig7 = px.bar(
-    speed_band,
-    x='latency_band',
-    y='production_speed_units_per_hr',
-    color='latency_band',
-    title="Speed by Latency Band"
-)
+    speed_band = df.groupby('latency_band')['production_speed_units_per_hr'].mean().reset_index()
 
-col1.plotly_chart(fig7, use_container_width=True)
+    fig7 = px.bar(
+        speed_band,
+        x='latency_band',
+        y='production_speed_units_per_hr',
+        color='latency_band',
+        title="Speed by Latency Band"
+    )
+
+    col1.plotly_chart(fig7, use_container_width=True)
+
+else:
+    st.error("Required columns not found")
 
 
 # Heatmap
